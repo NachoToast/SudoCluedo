f012d160cdf0142ad81d1ffdc7b9d43aeff4f2f8
tukib <ward.leikis@gmail.com>
2022-03-10 13:40:22 +1300

    client webpack and dev setup

---
+++ client/src/main.ts
@@ -0,0 +1,7 @@
+export class Test {
+    public constructor(private x: number) {
+        console.log(this.x);
+    }
+}
+
+new Test(5);
---
+++ client/webpack.config.js
@@ -0,0 +1,46 @@
+/* eslint-disable */
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: path.resolve(__dirname, 'src/main.ts'),
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build'),
+        clean: true,
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/index.html'),
+                    to: path.resolve(__dirname, 'build'),
+                },
+                {
+                    from: path.resolve(__dirname, 'src/assets'),
+                    to: path.resolve(__dirname, 'build/assets'),
+                    noErrorOnMissing: true,
+                },
+            ],
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'build'),
+        },
+        port: 3000,
+    },
+};
