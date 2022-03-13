46b39d7304ef9beb084b87fada079d5a40f18442
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-03-13 17:01:56 +1300

    add first endpoint test

---
+++ server/src/handlers/getNumLobbies.test.ts
@@ -0,0 +1,26 @@
+import { serverHub } from '..';
+import Lobby from '../classes/Lobby';
+import httpClient from '../tests/httpClient';
+
+describe('GET /numLobbies', () => {
+    afterAll(() => {
+        serverHub.close();
+    });
+
+    it('should return number of lobbies', async () => {
+        const { data: data0 } = await httpClient.get('/numLobbies');
+        expect(data0).toBe(0);
+
+        serverHub['_lobbies']['fakeLobbyA'] = {} as Lobby;
+
+        const { data: data1 } = await httpClient.get('/numLobbies');
+        expect(data1).toBe(1);
+
+        serverHub['_lobbies']['fakeLobbyB'] = {} as Lobby;
+        serverHub['_lobbies']['fakeLobbyC'] = {} as Lobby;
+        serverHub['_lobbies']['fakeLobbyD'] = {} as Lobby;
+
+        const { data: data2 } = await httpClient.get('/numLobbies');
+        expect(data2).toBe(4);
+    });
+});
